5c3484f889135c6f48f15c2a1712ac5c584302bd
ElChapoLSDC <71350293+ElChapoLSDC@users.noreply.github.com>
2020-10-28 20:11:50 +0100

    Update blacklistedevents.lua

--- tables/blacklistedevents.lua
+++ tables/blacklistedevents.lua
@@ -1,10 +1,6 @@
--- Here you are going to add all your blacklisted events, if you DON'T know what are you doing, leave it default and follow the tutorial
--- If you already know what im talking about, add everything to make your server safer
--- Don't forget commas!!
+ANTICHEAT = {}
 
-FuxiaBE = {}
-
-FuxiaBE.Events = {
+ANTICHEAT.Events = {
 	
 	--[[ -----
 	[!_script]
